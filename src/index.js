c6ea48a1a87f07f035c2052542dab314d19aeccd
lakdav <ladav2018@gmail.com>
2023-02-02 18:54:48 +0300

    solving the problem

--- src/index.js
+++ src/index.js
@@ -1,3 +1,35 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
-}
+	let cfb = bracketsConfig.flat();
+	let oB = cfb.filter((item, i) => i % 2 === 0);
+	let cB = cfb.filter((item, i) => i % 2 === 1);
+
+	let brackets = str.split('');
+
+	let cache = [];
+	let stack = [];
+
+	for (let i = 0; i < brackets.length; i++) {
+		o: for (let j = 0; j < oB.length; j++) {
+			if (oB[j] === cB[j]) {
+				if (brackets[i] === cache[cache.length - 1]) {
+					stack.pop();
+					cache.pop();
+				} else {
+					stack.push(brackets[i]);
+					cache.push(brackets[i]);
+				}
+				break o;
+			} else if (brackets[i] === oB[j]) {
+				stack.push(brackets[i]);
+				break o;
+			} else if (brackets[i] === cB[j]) {
+				let index = cfb.indexOf(cB[j]);
+				let tmp = cfb[index - 1];
+				if (tmp !== stack.pop()) return false;
+				break o;
+			}
+		}
+	}
+
+	return stack.length === 0 ? true : false;
+};
